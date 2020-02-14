634403f3bded5e219a2e6313581653ed00c4bdea
RAUL RIVERO RUBIO <5000928902@mwce111-10.local>
2020-02-13 18:34:58 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by RAUL RIVERO RUBIO on 2/13/20.
+//  Copyright © 2020 RAUL RIVERO RUBIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
